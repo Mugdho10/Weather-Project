cce7602ce2448c05e9f0cf26c3915206b64c2a0c
Mugdho <muhammadmugdho@gmail.com>
2025-06-12 03:57:42 +0600

    The temperature, description and the particular weather condition image is shown in the webpage after the latest updates

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ const port = 3000;
 
 app.get('/', (req, res) => {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&unit=metric&appid=623b63aa1fa0d3176a488e50c17b51b8"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=623b63aa1fa0d3176a488e50c17b51b8"
   
     https.get(url, (response) => {  
-        console.log(response);    
-    })
-  
-    res.send("App is running");
+        console.log(response); 
+
+        response.on('data', (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.send(`<h1>The temperature in London is ${temp}°C with ${weatherDescription}.</h1><img src="${imageURL}">`);
+        });
+    });
 });
 
 
